64b24732040dd6f15939ff8d0b98ae3e0c9b02e3
batiger00 <batiger00@gmail.com>
2022-07-09 17:45:41 +0900

    bus line page making

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,56 +8,90 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowMaterialGrid: false,
+      home: LinePage(),
+      title: 'BusHeXA-line-page',
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+class LinePage extends StatefulWidget {
+  const LinePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LinePage> createState() => _LinePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _LinePageState extends State<LinePage> {
+  final List<String> bus_num_list = ['337', '304', '743', '133', '233'];
+  final List dir_list = [
+    ['337(삼남 순환)', '337(삼남신화 방면)', '337(태화강역 방면)'],
+    ['304(율리방면)', '304(복합웰컴센터 방면)'],
+    ['743(태화강역 방면)', '743(울산과학기술원 방면)'],
+    ['133(꽃바위 방면)', '133(울산과학기술원 방면)'],
+    ['233(농소차고지 방면)', '233(농소차고지 방면)', '233(울산과학기술원 방면)']
+  ];
 
   @override
+  void ShowDirList(context, index) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+            child: ListView.builder(
+              shrinkWrap: true,
+              
+              padding: EdgeInsets.all(10),
+              itemCount: dir_list[index].length,
+              itemBuilder: (BuildContext context, int bus_num) {
+                return GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    margin: EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      '${dir_list[index][bus_num]}',
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Line Page'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: bus_num_list.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              ShowDirList(context, index);
+            },
+            child: Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              margin: EdgeInsets.only(bottom: 10),
+              child: Text(
+                '${bus_num_list[index]}',
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          );
+        },
       ),
     );
   }
