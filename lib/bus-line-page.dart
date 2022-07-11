8e93230939916cf0aae4151d885e7b5037b6d652
batiger00 <batiger00@gmail.com>
2022-07-11 14:56:18 +0900

    exchange names of var and func

--- lib/bus-line-page.dart
+++ lib/bus-line-page.dart
@@ -8,8 +8,8 @@ class LinePage extends StatefulWidget {
 }
 
 class _LinePageState extends State<LinePage> {
-  final List<String> bus_num_list = ['337', '304', '743', '133', '233'];
-  final List dir_list = [
+  final List<String> numbers = ['337', '304', '743', '133', '233'];
+  final List direction = [
     ['337(삼남 순환)', '337(삼남신화 방면)', '337(태화강역 방면)'],
     ['304(율리방면)', '304(복합웰컴센터 방면)'],
     ['743(태화강역 방면)', '743(울산과학기술원 방면)'],
@@ -17,7 +17,7 @@ class _LinePageState extends State<LinePage> {
     ['233(농소차고지 방면)', '233(농소차고지 방면)', '233(울산과학기술원 방면)']
   ];
 
-  void ShowDirList(context, index) {
+  void showDir(context, index) {
     showDialog(
         context: context,
         builder: (context) {
@@ -25,7 +25,7 @@ class _LinePageState extends State<LinePage> {
             child: ListView.builder(
               shrinkWrap: true,
               padding: EdgeInsets.all(10),
-              itemCount: dir_list[index].length,
+              itemCount: direction[index].length,
               itemBuilder: (BuildContext context, int bus_num) {
                 return GestureDetector(
                   onTap: () {},
@@ -38,7 +38,7 @@ class _LinePageState extends State<LinePage> {
                     ),
                     margin: EdgeInsets.only(bottom: 10),
                     child: Text(
-                      '${dir_list[index][bus_num]}',
+                      '${direction[index][bus_num]}',
                     ),
                   ),
                 );
@@ -55,11 +55,11 @@ class _LinePageState extends State<LinePage> {
       ),
       body: ListView.builder(
         padding: EdgeInsets.all(10),
-        itemCount: bus_num_list.length,
+        itemCount: numbers.length,
         itemBuilder: (BuildContext context, int index) {
           return GestureDetector(
             onTap: () {
-              ShowDirList(context, index);
+              showDir(context, index);
             },
             child: Container(
               alignment: Alignment.center,
@@ -70,7 +70,7 @@ class _LinePageState extends State<LinePage> {
               ),
               margin: EdgeInsets.only(bottom: 10),
               child: Text(
-                '${bus_num_list[index]}',
+                '${numbers[index]}',
               ),
             ),
           );
