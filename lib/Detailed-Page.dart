82d7cdcdbba31a84325a9340b4b9ddf072ac95f0
Yunjigong <rhddbswl0420@naver.com>
2022-07-14 15:05:10 +0900

    0714 상세페이지
    수정사항
    1. 변수에 따라서 노선명, 주요역값이 바뀌도록 했습니다.
    2. 시간표 list 임의로 만들어서 변수에 따라서 그 노선에 맞는 모든 시간이 나타나도록 했습니다.
    3. 노선에서 datarow 부분의 색상을 번갈아서 나타나도록 했습니다.
    4. 버스 각 노선의 정류장을 리스트로 받으면 그 정류장들을 data table 에 순서대로 표시하도록 했습니다.

--- lib/Detailed-Page.dart
+++ lib/Detailed-Page.dart
@@ -1,17 +1,44 @@
-import 'dart:html';
 import 'package:flutter/material.dart';
 
-class DetailedPage extends StatefulWidget {
-  const DetailedPage({ Key? key }) : super(key: key);
+class detailedPage extends StatefulWidget {
+  const detailedPage({ Key? key }) : super(key: key);
 
   @override
-  State<DetailedPage> createState() => _DetailedPageState();
+  State<detailedPage> createState() => _detailedPageState();
 }
 
-class _DetailedPageState extends State<DetailedPage> {
-  final List<String> _RouteList = ['337(삼남 순환)'];
-  final List<String> _MainStation = ['KTX울산역, 구영리, 언양터미널, 울산터미널'];
-  final List<String> _TimeTable = ['05:50'];
+class _detailedPageState extends State<detailedPage> {
+  final List _routeList = [
+    ['337(삼남 순환)', '337(삼남신화 방면)', '337(태화강역 방면)'],
+    ['304(율리방면)', '304(복합웰컴센터 방면)'],
+    ['743(태화강역 방면)', '743(울산과학기술원 방면)'],
+    ['133(꽃바위 방면)', '133(울산과학기술원 방면)'],
+    ['233(농소차고지 방면)', '233(농소차고지 방면)', '233(울산과학기술원 방면)']
+  ];
+
+  final List _mainStation = [
+    'KTX울산역, 구영리, 언양터미널, 울산터미널',
+    'KTX울산역, 신복로터리, 언양터미널, 울산대',
+    '공업탑, 산단캠, 신복로터리, 울산대, 울산터미널',
+    '구영리, 울산터미널',
+    '공업탑, 구영리, 신복로터리, 울산대',
+    '구영리'
+  ];
+
+  List _timeTable = [
+    [['05:35','06:05','06:30'],[''],['']],
+    [[''],['']],
+    [[''],['']],
+    [[''],['']],
+    [[''],['']],
+    [[''],[''],['']]];
+
+
+   List <dynamic>_busLine = ['삼남신화','마산','웃마산','마산입구'];
+  
+  int idx = 0;
+  int bus_num=0;
+
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +63,7 @@ class _DetailedPageState extends State<DetailedPage> {
                     height: 40,
                     padding: EdgeInsets.all(10.0),
                     decoration: BoxDecoration(color: Colors.purpleAccent.withOpacity(0.1)),
-                    child: Text('노선명: '+ _RouteList[0]),
+                    child: Text('노선명: ${_routeList[idx][bus_num]}'),
                   ),
                   ),
             ],
@@ -49,13 +76,13 @@ class _DetailedPageState extends State<DetailedPage> {
                 borderRadius: BorderRadius.circular(5.0),
                 border: Border.all(color: Colors.black12),
               ),
-              child: Text('주요역: '+ _MainStation[0]),
+              child: Text('주요역: ${_mainStation[idx]}')
             ),
             ExpansionTile(
               title: const Text('시간표'),
               children: <Widget>[
-                ListTile(title: Text(_TimeTable[0]),)
-              ],
+                ListTile(title: Text('${_timeTable[idx][bus_num].join('   ')}'))
+                ],
                 ), 
             Container(
               width: double.infinity,
@@ -67,20 +94,28 @@ class _DetailedPageState extends State<DetailedPage> {
               ),
               child: DataTable(
                 headingRowColor: MaterialStateColor.resolveWith((states)=>Colors.blue),
-                dataRowColor: MaterialStateColor.resolveWith((states) => Colors.blueAccent.withOpacity(0.1)),
                 columns:[
               DataColumn(label: Text('0')),
               DataColumn(label: Text('노선')),
               DataColumn(label: Text('운행중')),
             ], 
-             rows: [
-              DataRow(cells: [
-                DataCell(Text('node_order')),
-                DataCell(Text('node_name')),
-                DataCell(Text(''))
-              ]),
-            ])
-              ),
+             rows: List<DataRow>.generate(_busLine.length,
+              (index) => DataRow(
+                color: MaterialStateProperty.resolveWith<Color>(
+                                (Set<MaterialState> states) {
+                              if (index % 2 == 0)
+                                return Colors.transparent;
+                              return  Colors.blueAccent.withOpacity(0.1);
+                            }),
+                           cells: [
+                              DataCell(Text((index+1).toString())),
+                              DataCell(Text(_busLine[index].toString())),
+                              DataCell(Text(''))
+                           ].toList(),
+                    ),
+                  )
+                )
+              )
             ]
           )
         )
