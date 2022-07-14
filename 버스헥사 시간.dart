d6b2bffa857b6274cfd387921e6a7ce564a8781e
GwangHo123 <105975660+GwangHo123@users.noreply.github.com>
2022-07-14 12:48:09 +0900

    Update 버스헥사 시간.dart

--- 버스헥사 시간.dart
+++ 버스헥사 시간.dart
@@ -3,7 +3,135 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Map> _Bustime = [
+    {
+      'time': '05:35',
+      'busnumber': '337',
+      'busdiretion': '삼남 순환',
+      'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
+    },
+    {
+      'time': '06:05',
+      'busnumber': '337',
+      'busdiretion': '삼남 순환',
+      'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
+    },
+    {
+      'time': '06:30',
+      'busnumber': '337',
+      'busdiretion': '삼남 순환',
+      'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
+    },
+    {
+      'time': '07:00',
+      'busnumber': '337',
+      'busdiretion': '삼남 순환',
+      'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
+    },
+    {
+      'time': '07:40',
+      'busnumber': '337',
+      'busdiretion': '삼남 순환',
+      'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
+    },
+    {
+      'time': '08:20',
+      'busnumber': '337',
+      'busdiretion': '삼남 순환',
+      'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
+    },
+    {
+      'time': '09:05',
+      'busnumber': '337',
+      'busdiretion': '삼남 순환',
+      'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
+    },
+    {
+      'time': '09:35',
+      'busnumber': '337',
+      'busdiretion': '삼남 순환',
+      'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
+    },
+    {
+      'time': '10:10',
+      'busnumber': '337',
+      'busdiretion': '삼남 순환',
+      'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
+    },
+    {
+      'time': '10:45',
+      'busnumber': '337',
+      'busdiretion': '삼남 순환',
+      'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
+    },
+    {
+      'time': '11:25',
+      'busnumber': '337',
+      'busdiretion': '삼남 순환',
+      'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
+    },
+    {
+      'time': '12:05',
+      'busnumber': '337',
+      'busdiretion': '삼남 순환',
+      'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
+    },
+    {
+      'time': '12:40',
+      'busnumber': '337',
+      'busdiretion': '삼남 순환',
+      'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('버스출발시간 기준                         현재시각: t'),
+        ),
+        body: ListView(
+          children: [_createDataTable()],
+        ),
+      ),
+    );
+  }
+
+  DataTable _createDataTable() {
+    return DataTable(columns: _createColumns(), rows: _createRows());
+  }
+
+  List<DataColumn> _createColumns() {
+    return [
+      DataColumn(label: Text('시간')),
+      DataColumn(label: Text('버스번호')),
+      DataColumn(label: Text('노선방면')),
+      DataColumn(label: Text('노선경유'))
+    ];
+  }
+
+  List<DataRow> _createRows() {
+    return _Bustime.map((bustime) => DataRow(cells: [
+          DataCell(Text(bustime['time'])),
+          DataCell(Text(bustime['busnumber'])),
+          DataCell(Text(bustime['busdiretion'])),
+          DataCell(Text(bustime['route'])),
+        ])).toList();
+  }
+}
+: super(key: key);
 
   static const String _title =
       'Bus Hexa                                     현재시각: t';
