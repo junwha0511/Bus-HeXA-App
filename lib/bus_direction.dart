6492f84235bdc75e9a75e4500fb3d8db3028aa2b
준화 <junwha0511@naver.com>
2022-07-21 12:07:24 +0900

    Disable unused file

---
+++ lib/bus_direction.dart
@@ -0,0 +1,16 @@
+// import 'package:flutter/src/foundation/key.dart';
+// import 'package:flutter/src/widgets/framework.dart';
+
+// class BusDetailPage extends StatefulWidget {
+//   const BusDetailPage({Key? key}) : super(key: key);
+
+//   @override
+//   State<BusDetailPage> createState() => _BusDetailPageState();
+// }
+
+// class _BusDetailPageState extends State<BusDetailPage> {
+//   @override
+//   Widget build(BuildContext context) {
+    
+//   }
+// }
