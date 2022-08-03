f5a576f189a47f566c2168ceddd8a6938dd6a694
Yunjigong <rhddbswl0420@naver.com>
2022-08-04 02:21:35 +0900

    0804-출발시간기준
    stream을 아직 이해하지 못해서 일단 format만 하고 commit합니다!

--- lib/bustime.dart
+++ lib/bustime.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:developer';
+import 'dart:html';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -5,16 +8,41 @@ import 'package:flutter/material.dart';
 void main() => runApp(BusTime());
 
 class BusTime extends StatefulWidget {
+  const BusTime({Key? key}) : super(key: key);
+
   @override
-  _BusTimeState createState() => _BusTimeState();
+  State<BusTime> createState() => _BusTimeState();
 }
 
 class _BusTimeState extends State<BusTime> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'busDeparatureTime',
+      home: deparatureTime(),
+      debugShowCheckedModeBanner: false,
+      builder: (context, childWidget) {
+        return MediaQuery(
+            data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+            child: childWidget!);
+      },
+    );
+  }
+}
+
+class deparatureTime extends StatefulWidget {
+  const deparatureTime({Key? key}) : super(key: key);
+
+  @override
+  State<deparatureTime> createState() => _deparatureTimeState();
+}
+
+class _deparatureTimeState extends State<deparatureTime> {
   List<Map> _busTime = [
     {
       'hour': 05,
       'minute': 35,
-      'time':'05:35',
+      'time': '05:35',
       'busnumber': '337',
       'busdiretion': '삼남 순환',
       'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
@@ -22,7 +50,7 @@ class _BusTimeState extends State<BusTime> {
     {
       'hour': 06,
       'minute': 05,
-      'time':'06:05',
+      'time': '06:05',
       'busnumber': '337',
       'busdiretion': '삼남 순환',
       'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
@@ -30,7 +58,7 @@ class _BusTimeState extends State<BusTime> {
     {
       'hour': 06,
       'minute': 30,
-      'time':'06:30',
+      'time': '06:30',
       'busnumber': '337',
       'busdiretion': '삼남 순환',
       'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
@@ -38,7 +66,7 @@ class _BusTimeState extends State<BusTime> {
     {
       'hour': 07,
       'minute': 00,
-      'time':'07:00',
+      'time': '07:00',
       'busnumber': '337',
       'busdiretion': '삼남 순환',
       'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
@@ -46,7 +74,7 @@ class _BusTimeState extends State<BusTime> {
     {
       'hour': 07,
       'minute': 40,
-      'time':'07:40',
+      'time': '07:40',
       'busnumber': '337',
       'busdiretion': '삼남 순환',
       'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
@@ -54,7 +82,7 @@ class _BusTimeState extends State<BusTime> {
     {
       'hour': 08,
       'minute': 20,
-      'time':'08:20',
+      'time': '08:20',
       'busnumber': '337',
       'busdiretion': '삼남 순환',
       'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
@@ -62,7 +90,7 @@ class _BusTimeState extends State<BusTime> {
     {
       'hour': 09,
       'minute': 05,
-      'time':'09:05',
+      'time': '09:05',
       'busnumber': '337',
       'busdiretion': '삼남 순환',
       'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
@@ -70,7 +98,7 @@ class _BusTimeState extends State<BusTime> {
     {
       'hour': 09,
       'minute': 35,
-      'time':'09:35',
+      'time': '09:35',
       'busnumber': '337',
       'busdiretion': '삼남 순환',
       'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
@@ -78,7 +106,7 @@ class _BusTimeState extends State<BusTime> {
     {
       'hour': 10,
       'minute': 10,
-      'time':'10:10',
+      'time': '10:10',
       'busnumber': '337',
       'busdiretion': '삼남 순환',
       'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
@@ -86,7 +114,7 @@ class _BusTimeState extends State<BusTime> {
     {
       'hour': 10,
       'minute': 45,
-      'time':'10:45',
+      'time': '10:45',
       'busnumber': '337',
       'busdiretion': '삼남 순환',
       'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
@@ -94,7 +122,7 @@ class _BusTimeState extends State<BusTime> {
     {
       'hour': 11,
       'minute': 25,
-      'time':'11:25',
+      'time': '11:25',
       'busnumber': '337',
       'busdiretion': '삼남 순환',
       'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
@@ -102,7 +130,7 @@ class _BusTimeState extends State<BusTime> {
     {
       'hour': 12,
       'minute': 05,
-      'time':'12:05',
+      'time': '12:05',
       'busnumber': '337',
       'busdiretion': '삼남 순환',
       'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
@@ -110,117 +138,92 @@ class _BusTimeState extends State<BusTime> {
     {
       'hour': 12,
       'minute': 40,
-      'time':'12:40',
+      'time': '12:40',
       'busnumber': '337',
       'busdiretion': '삼남 순환',
       'route': '울산터미널, 구영리, 언양터미널, KTX울산역'
     },
   ];
 
-TimeOfDay time = TimeOfDay(hour: 00, minute: 00);
+  var newList = new List.empty(growable: true);
+  TimeOfDay time = TimeOfDay(hour: 00, minute: 00);
 
-void _showTimePicker(){
-  showTimePicker(context: context,
-   initialTime: time).then((value) {
-    setState(() {
-      time=value!;
+  void _showTimePicker() {
+    showTimePicker(context: context, initialTime: time).then((value) {
+      setState(() {
+        time = value!;
+      });
     });
-   });
-}
+  }
 
   @override
   Widget build(BuildContext context) {
- 
-  
-var splitTime = [];
-var timeOfBusTime = [];
-var hourOfTime = new List.empty(growable: true);
-var minuteOfTime = new List.empty(growable: true);
-
-for (var i =0; i<_busTime.length; i++) {
-  splitTime = _busTime[i]['time'].split(':');
-  hourOfTime.add(splitTime[0]);
-  minuteOfTime.add(splitTime[1]);
-  splitTime=[];
-}
-
-var newList = new List.empty(growable: true);
-
-for (var i =0; i<_busTime.length; i++) {
-  if(hourOfTime[i] >= time.hour){
-    if(minuteOfTime[i]>=time.minute){
-      newList.add(_busTime[i]);
+    for (var i = 0; i < _busTime.length; i++) {
+      if (_busTime[i]['hour'] >= time.hour) {
+        if (_busTime[i]['minute'] >= time.minute) {
+          newList.add(_busTime[i]);
+        }
+      }
     }
-  };
-}
 
-    return MaterialApp(
-      home:Scaffold(
-        appBar: AppBar(
-          title: Text('버스출발시간 기준'),
-        ),
-        body: SafeArea(
-          child: Center(
-            child: SingleChildScrollView(
-              child: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('버스출발시간 기준'),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                    Container(
-                      height: 40,
-                      width: 5,
-                      padding: EdgeInsets.symmetric(vertical: 5.0),
-                      decoration: BoxDecoration(color: Colors.purple[700]),
-                    ),
-                    Expanded(child: Container(
-                      height: 40,
-                      padding: EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(color: Colors.purpleAccent.withOpacity(0.1)),
-                      child: Row(
-                        children: [
-                          Text(
-                        '현재 시각: ${time.hour.toString().padLeft(2,'0')}:${time.minute.toString().padLeft(2, '0')} '
-                      ),
-                      const SizedBox( width: 5,),
-                     TextButton(child: Text('시간 선택'),
-                     onPressed:(){
-                      showDialog(context: context,
-                      barrierDismissible: true,
-                       builder: (BuildContext context){
-                        return AlertDialog(
-                          content: Row(children: [
-                            Text('hi')
-                          ],),
-                          actions: [
-                            TextButton(child: const Text('확인'),
-                            onPressed: (){
-                              Navigator.of(context).pop();
-                            },)
+                  Container(
+                    height: 40,
+                    width: 5,
+                    padding: EdgeInsets.symmetric(vertical: 5.0),
+                    decoration: BoxDecoration(color: Colors.purple[700]),
+                  ),
+                  Expanded(
+                    child: Container(
+                        height: 40,
+                        padding: EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                            color: Colors.purpleAccent.withOpacity(0.1)),
+                        child: Row(
+                          children: [
+                            Text(
+                                '현재 시각: ${time.hour.toString().padLeft(2, '0')}:${time.minute.toString().padLeft(2, '0')} '),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            OutlinedButton(
+                              child: Text('시간 선택'),
+                              onPressed: () {
+                                showTimePicker(
+                                        context: context, initialTime: time)
+                                    .then((value) {
+                                  setState(() {
+                                    time = value!;
+                                  });
+                                });
+                              },
+                              style:
+                                  TextButton.styleFrom(primary: Colors.black),
+                            )
                           ],
-                        );
-                       });
-                     })
-                        ],
-                      )
-                    ),
-                    ),
-              ],
+                        )),
+                  ),
+                ],
               ),
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
-                child: Container(
-                  child: _createDataTable(),
-                )
+                child: Container(child: _createDataTable()),
               ),
-              
-                ],
-              ),
-            ),
+            ],
           ),
         ),
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 
@@ -238,12 +241,14 @@ for (var i =0; i<_busTime.length; i++) {
   }
 
   List<DataRow> _createRows() {
-    return _busTime.map((newList) => DataRow(cells: [
-          DataCell(Text('${newList['hour'].toString().padLeft(2,'0')}:${newList['minute'].toString().padLeft(2,'0')}')),
-          DataCell(Text(newList['busnumber'])),
-          DataCell(Text(newList['busdiretion'])),
-          DataCell(Text(newList['route'])),
-        ])).toList();
+    return newList
+        .map((newList) => DataRow(cells: [
+              DataCell(Text(
+                  '${newList['hour'].toString().padLeft(2, '0')}:${newList['minute'].toString().padLeft(2, '0')}')),
+              DataCell(Text(newList['busnumber'])),
+              DataCell(Text(newList['busdiretion'])),
+              DataCell(Text(newList['route'])),
+            ]))
+        .toList();
   }
-
 }
